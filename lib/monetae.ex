dd38d58ea5ef3aa1d00a6c4fcdc49ad0a04a2ef0
Brian Edwards <bedwards@indeed.com>
2021-02-12 10:49:02 -0600

    Init commit

---
+++ lib/monetae.ex
@@ -0,0 +1,18 @@
+defmodule Monetae do
+  @moduledoc """
+  Documentation for `Monetae`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Monetae.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/monetae_test.exs
@@ -0,0 +1,8 @@
+defmodule MonetaeTest do
+  use ExUnit.Case
+  doctest Monetae
+
+  test "greets the world" do
+    assert Monetae.hello() == :world
+  end
+end
